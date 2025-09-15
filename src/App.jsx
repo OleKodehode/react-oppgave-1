e4fb1708d9b2ed8eabb13b5cf0eb351159d4e9a0
Ole Kodehode <ole.kodehode@gmail.com>
2025-09-15 11:17:34 +0200

    sorted by category

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import MenuComponent from "./components/MenuComponent";
 import { meny } from "./menu_data";
 
 function App() {
-  return <>{<MenuComponent menu={meny} />}</>;
+  const categoryLookup = {
+    Forrett: 0,
+    Hovedrett: 1,
+    Dessert: 2,
+  };
+  // Sort the menu based on the value in the lookup object - Sort by category.
+  const sortedMenu = meny.sort(
+    (a, b) => categoryLookup[a.kategori] - categoryLookup[b.kategori]
+  );
+  return <>{<MenuComponent menu={sortedMenu} />}</>;
 }
 
 export default App;
--- src/components/DishComponent.jsx
+++ src/components/DishComponent.jsx
@@ -3,7 +3,7 @@ import styles from "../css/DishContainer.module.css";
 export default function DishComponent(dish) {
   return (
     <>
-      <li id={dish.id} className={styles.kategori}>
+      <li id={dish.id} className={styles.card}>
         <p className={styles.category}>{dish.kategori}</p>
         <p className={styles.title}>{dish.tittel}</p>
         <p className={styles.price}>{dish.pris}</p>
